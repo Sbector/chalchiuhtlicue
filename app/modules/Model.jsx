1829c69c76f6c40a7de9fd59884306d49c582981
Sbector <saulbector@gmail.com>
2024-07-26 19:54:28 -0600

    remove autorotation

--- app/modules/Model.jsx
+++ app/modules/Model.jsx
@@ -3,11 +3,7 @@ import { Suspense, useRef } from "react";
 import { useFrame } from "@react-three/fiber"
 
 export default function Model() {
-  const ref = useRef();
-  useFrame((_,delta)=> {
-    ref.current.rotation.x += 0.015* delta
-    ref.current.rotation.y += 0.015* delta
-  })
+  
   const url = "./models/chalchiuhtlicueWeb.glb";
   const gltf = useGLTF(url);
   console.log(gltf);
@@ -15,7 +11,7 @@ export default function Model() {
   return (
     <Suspense>
       <primitive
-        ref = {ref}
+        
         object={gltf.scene}
         position={[0, -8, 0]}
         scale={[1, 1, 1]}
